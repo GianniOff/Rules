ca7a92fed7f2f8230c11c8a1d8d99f00b5038ec8
𝗚𝗶𝗮𝗻𝗻𝗶 🍓 <zikmuytb3@gmail.com>
2026-03-13 20:27:59 +0100

    Update and rename data.js to serveur.js

---
+++ G/serveur.js
@@ -0,0 +1,11 @@
+if (!window.ReglementsData) window.ReglementsData = {};
+
+window.ReglementsData['serveur'] = {
+    nom: "Règles du Serveur",
+    description: "Les règles techniques, l'utilisation des salons et les litiges.",
+    derniereMiseAJour: "10 Mars 2026",
+    articles: [
+        { id: 1, titre: "Utilisation des salons", contenu: "Postez vos messages dans les bons canaux." },
+        { id: 2, titre: "Publicité", contenu: "Les liens externes sont interdits sans autorisation." }
+    ]
+};
