8a3cfe6dd595c396d530a06cf933f6f77db7f144
𝗚𝗶𝗮𝗻𝗻𝗶 🍓 <zikmuytb3@gmail.com>
2026-03-15 20:06:47 +0100

    Update discord.js

--- G/discord.js
+++ G/discord.js
@@ -1,11 +1,13 @@
 if (!window.ReglementsData) window.ReglementsData = {};
 
 window.ReglementsData['serveur'] = {
-    nom: "Règles du Serveur",
-    description: "Les règles techniques, l'utilisation des salons et les litiges.",
+    nom: "Discord règlement relatif a Gianni",
+    description: "Règles pour l'utilisation de Discord",
     derniereMiseAJour: "10 Mars 2026",
     articles: [
-        { id: 1, titre: "Utilisation des salons", contenu: "Postez vos messages dans les bons canaux." },
-        { id: 2, titre: "Publicité", contenu: "Les liens externes sont interdits sans autorisation." }
+        { id: 1, titre: "Signalement", contenu: "Concernant les signalements au staff gianni ne peux signaler un membre ou un problème de légalité dans quelle que chose ne le concernant pas" },
+        { id: 2, titre: "Questions", contenu: "Les questions posée a un membre du staff d'un serveur n'ayant pas d'importance dans l'immediat ne peuvent être posée par Gianni" },
+        { id: 4, titre: "Conditions d'adhésion a un serveur", contenu: "Pour que Gianni rejoignent un serveur il faut qu'une de ces conditions sois respecté : [Le serveur doit avoir plus de 10 mil vrai membre]" },
+        { id: 3, titre: "Ban", contenu: "En cas de bannisement d'un serveur les demandes de déban sont exclu des possibilité sauf si le bannisement n'est pas justifiée mais Gianni peut tout de même demandé des informations sur sont bannisement" }
     ]
 };
